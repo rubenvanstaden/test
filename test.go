7f69f9e1f97d2d67dd51b57ff484add31bc9b42c
Ruben van Staden <rubenvanstaden@gmail.com>
2022-11-15 10:54:18 -0500

    refactor: re-add print position to capture caller data

--- test.go
+++ test.go
@@ -3,6 +3,8 @@ package test
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"path/filepath"
 	"reflect"
 	"testing"
 
@@ -12,26 +14,34 @@ import (
 // Fails the test if the condition is false.
 func Assert(tb testing.TB, condition bool, msg string) {
 	if !condition {
-        out := fmt.Sprintf(color.InRed("\nmsg: %s\n"), msg)
-		tb.Errorf(out)
+        _, file, line, _ := runtime.Caller(1)
+        out := fmt.Sprintf(color.InRed("%s:%d\n\n"), filepath.Base(file), line)
+        out += fmt.Sprintf(color.InRed("\tmsg: %s\n\n"), msg)
+        fmt.Printf(out)
+		tb.Fail()
 	}
 }
 
 // Fails the test if an err is not nil.
 func Ok(tb testing.TB, err error) {
 	if err != nil {
-        out := fmt.Sprintf(color.InRed("\ncatch: %s\n"), err.Error())
-		tb.Errorf(out)
+        _, file, line, _ := runtime.Caller(1)
+        out := fmt.Sprintf(color.InRed("%s:%d\n\n"), filepath.Base(file), line)
+        out += fmt.Sprintf(color.InRed("\tcatch: %s\n\n"), err.Error())
+        fmt.Printf(out)
+		tb.Fail()
 	}
 }
 
 // Fails the test if "want" is not equal to "got".
 func Equals(tb testing.TB, want, got interface{}) {
 	if !reflect.DeepEqual(want, got) {
-        want := fmt.Sprintf(color.InRed("\nwant: %#v\n"), want)
-        got := fmt.Sprintf(color.InRed("\ngot: %#v\n"), got)
-        out := want + got
-		tb.Errorf(out)
+        _, file, line, _ := runtime.Caller(1)
+        out := fmt.Sprintf(color.InRed("%s:%d\n\n"), filepath.Base(file), line)
+        out += fmt.Sprintf(color.InRed("\twant: %#v\n\n"), want)
+        out += fmt.Sprintf(color.InRed("\tgot: %#v\n\n"), got)
+        fmt.Printf(out)
+		tb.Fail()
 	}
 }
 
@@ -39,7 +49,10 @@ func Equals(tb testing.TB, want, got interface{}) {
 func Exists(tb testing.TB, filename string) {
 	_, err := os.Stat(filename)
 	if os.IsNotExist(err) {
-        out := fmt.Sprintf(color.InRed("\nno file: %s\n"), filename)
-		tb.Errorf(out)
+        _, file, line, _ := runtime.Caller(1)
+        out := fmt.Sprintf(color.InRed("%s:%d\n\n"), filepath.Base(file), line)
+        out += fmt.Sprintf(color.InRed("\tno file: %s\n\n"), filename)
+        fmt.Printf(out)
+		tb.Fail()
 	}
 }
